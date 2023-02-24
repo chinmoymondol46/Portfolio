08f008d9a2c7bc70cd981a6a75df6c9cb68c1439
chinmoy-pc-home <chinmoy.mondol@tigerit.com>
2023-02-24 23:50:39 +0600

    Added react-icons to the header component

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/" target="_blank"></a>
-        <a href="https://www.github.com/" target="_blank"></a>
+        <a href="https://www.linkedin.com/" target="_blank"><BsLinkedin/></a>
+        <a href="https://www.github.com/" target="_blank"><BsGithub/></a>
     </div>
   )
 }
