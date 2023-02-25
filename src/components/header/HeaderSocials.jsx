c6771964a8fe2cb26cbcf92d2342d0ad6b0dfb7a
chinmoy-pc-home <chinmoy.mondol@tigerit.com>
2023-02-25 17:24:08 +0600

    Updated the necessary assets for the header component

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/" target="_blank"><BsLinkedin/></a>
-        <a href="https://www.github.com/" target="_blank"><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/chinmoymondol" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/chinmoymondol46" target="_blank"><BsGithub/></a>
     </div>
   )
 }
