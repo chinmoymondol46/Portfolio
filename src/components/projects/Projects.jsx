68344e101accd8ca638b4ed767c004aa7c7a2de9
chinmoy-pc-home <chinmoy.mondol@tigerit.com>
2023-03-04 19:46:40 +0600

    - Ranmed CSS classes for the 'Project' section - Fixed a responsive design issue where a big gap was getting generated for the tablet and mobile view

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -13,69 +13,69 @@ const Projects = () => {
       <h5>My Works</h5>
       <h2>Personal Projects</h2>
 
-      <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-img'>
+      <div className='container project__container'>
+        <article className='project__item'>
+          <div className='project__item-img'>
             <img src={IMG1} alt="Keyword Density Checker - C#"/>
           </div>
 
           <h3>Keyword Density Checker - C#</h3>
-          <div className='portfolio__item-cta'>
+          <div className='project__item-cta'>
             <a href="https://github.com/chinmoymondol46/Keyword-Density-Checker-CSharp" className='btn' target='_blank'>GitHub</a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-img'>
+        <article className='project__item'>
+          <div className='project__item-img'>
             <img src={IMG2} alt="Flower Classification Model - ML"/>
           </div>
 
           <h3>Flower Classification Model - ML</h3>
-          <div className='portfolio__item-cta'>
+          <div className='project__item-cta'>
             <a href="https://github.com/chinmoymondol46/Flower-Classification-Model-CNN" className='btn' target='_blank'>GitHub</a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-img'>
+        <article className='project__item'>
+          <div className='project__item-img'>
             <img src={IMG3} alt="Online Job Portal - PHP, JS"/>
           </div>
 
           <h3>Online Job Portal - PHP, JS</h3>
-          <div className='portfolio__item-cta'>
+          <div className='project__item-cta'>
             <a href="https://github.com/chinmoymondol46/Online-Job-Portal" className='btn' target='_blank'>GitHub</a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-img'>
+        <article className='project__item'>
+          <div className='project__item-img'>
             <img src={IMG4} alt="Brick Breaker Game - C++"/>
           </div>
 
           <h3>Brick Breaker Game - C++</h3>
-          <div className='portfolio__item-cta'>
+          <div className='project__item-cta'>
             <a href="https://github.com/chinmoymondol46/Brick-Breaker-Game-CPP" className='btn' target='_blank'>GitHub</a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-img'>
+        <article className='project__item'>
+          <div className='project__item-img'>
             <img src={IMG5} alt="Hospital Management System - C#"/>
           </div>
 
           <h3>Hospital Management System - C#</h3>
-          <div className='portfolio__item-cta'>
+          <div className='project__item-cta'>
             <a href="https://github.com/chinmoymondol46/Hospital-Management-System-CSharp" className='btn' target='_blank'>GitHub</a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-img'>
+        <article className='project__item'>
+          <div className='project__item-img'>
             <img src={IMG6} alt="Super Shop Management System - Java"/>
           </div>
 
           <h3>Super Shop Management System - Java</h3>
-          <div className='portfolio__item-cta'>
+          <div className='project__item-cta'>
             <a href="https://github.com/chinmoymondol46/Super-Shop-Management-System-Java" className='btn' target='_blank'>GitHub</a>
           </div>
         </article>
