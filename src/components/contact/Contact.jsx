580ab34fdd6c67424b6a8bd7b661bc06c3f41dc0
chinmoy-pc-home <chinmoy.mondol@tigerit.com>
2023-03-04 20:33:14 +0600

    Updated the 'Contact' section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './contact.css'
+import {AiOutlineMail} from 'react-icons/ai'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Gert In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>chinmoymondol46@gmail.com</h5>
+            <a href="mailto:chinmoymondol46@gmail.com">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
