e10461226ddc33829adbfe098b5195a592337507
chinmoy-pc-home <chinmoy.mondol@tigerit.com>
2024-10-17 12:25:08 +0600

    Updated the 'About', 'Header' sections accoding to promotion and added a new CV

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className='container header__container'>
         <h5>Hello, I'm</h5>
         <h1>Chinmoy Mondol</h1>
-        <h5 className='text-light'>Software Engineer (QA)</h5>
+        <h5 className='text-light'>Senior Software Engineer (QA)</h5>
         
         <CTA/>
         <HeaderSocials/>
